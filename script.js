de863b7bf53d72371fb1d679f8ee1c0591ce42f9
Velmoren <47055141+Velmoren@users.noreply.github.com>
2020-05-27 20:36:55 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var myHeaders = new Headers();
+myHeaders.append("Authorization", "Basic aHM6MXFhc3cyM2VkIQ==");
+
+var requestOptions = {
+ method: "GET",
+ headers: myHeaders,
+ redirect: "follow",
+};
+
+fetch(
+ "http://web1c.standart.by/std_UT/hs/api/StandartsByType/1",
+ requestOptions
+)
+ .then((response) => response.text())
+ .then((result) => console.log(result))
+ .catch((error) => console.log("error", error));
